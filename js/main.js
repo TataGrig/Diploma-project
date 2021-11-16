1bfefd90c4031343fd26acb83fcf89142171d173
Tatiana <priceless0816@gmail.com>
2021-11-15 19:28:06 -0800

    create: best & categories bloks, media for best

---
+++ js/main.js
@@ -0,0 +1,59 @@
+const menuButton = document.querySelector(".navbar-burger");
+
+  menuButton.addEventListener('click', 
+  function () {
+    console.log('Клик по кнопке меню');
+    const navbarMenu = document.querySelector(".navbar-menu");
+    navbarMenu.classList.toggle("navbar-menu_visible");
+    const body = document.querySelector("body");
+    body.classList.toggle("body_stop_scrolling");
+  });
+
+const likeButtons = document.querySelectorAll(".recommend__book-like");
+
+
+  likeButtons.forEach(likeButton=>{
+    likeButton.addEventListener('click', 
+      function () {
+        console.log('Клик по кнопке меню');
+        likeButton.classList.toggle("recommend__book-like_red");
+      }
+    );
+  })
+  
+
+const swiper = new Swiper('.swiper', {
+  loop: false,
+  autoHeight: true,
+  slidesPerView: 4,
+  spaceBetween: 26,
+  
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-arrow--right',
+    prevEl: '.swiper-arrow--left',
+  },
+
+  // breakpoints: {
+  //     1200: {
+  //       slidesPerView: 4,
+  //       loopedSlides: 4,
+  //       spaceBetween: 10
+  //     },
+  //     992: {
+  //       slidesPerView: 3,
+  //       loopedSlides: 3,
+  //       spaceBetween: 10
+  //     },
+  //     768: {
+  //       slidesPerView: 2,
+  //       loopedSlides: 2,
+  //       spaceBetween: 10
+  //     },
+  //     576: {
+  //       slidesPerView: 1,
+  //       loopedSlides: 1,
+  //       spaceBetween: 20,
+  //     }
+  //   }
+});
